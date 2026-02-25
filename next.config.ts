e9e5be42e93e2957d43371d41e9646a2628f2cfd
Djdan-byte <daniel1gon2ram@gmail.com>
2026-02-24 22:24:44 -0600

    intento 1000

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/stagebook-web', 
+  trailingSlash: true,
   images: {
     unoptimized: true,
   }
